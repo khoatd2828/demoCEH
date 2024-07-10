cf18b34a983eca1ff2d41c058f696b2b82b2e1ed
Khoa <dangkhoa020228@gmail.com>
2024-07-10 10:48:07 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import './main.scss'
-import './styles/tainwind.css'
 import { Login } from './assets/components/Login/Login'
 import { MainLayout } from './assets/components/layouts/MainLayout'
 import { AuthLayout } from './assets/components/layouts/AuthLayout'
 import { DashBoard } from './assets/components/DashBoard/DashBoard'
 import { Register } from './assets/components/Register/Register'
+import './App.css'
+import './main.scss'
+import './styles/tainwind.css'
 
 function App() {
 
